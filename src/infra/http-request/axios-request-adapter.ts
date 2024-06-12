640b496af82c678cef25f15169affdc018f782f9
Eduardo Schork <eduardo.schork1814@gmail.com>
2024-06-11 23:52:33 -0300

    chore: add axios ip comment

--- src/infra/http-request/axios-request-adapter.ts
+++ src/infra/http-request/axios-request-adapter.ts
@@ -3,6 +3,8 @@ import Axios, { AxiosInstance, AxiosRequestHeaders } from 'axios';
 
 import { HttpRequestParams, IHttpRequestPort } from './http-request-port';
 
+// Local machine IP if running app in dev mode
+// const API_BASE_URL = 'http://192.168.4.12:3000';
 const API_BASE_URL = 'http://52.67.103.199:3000';
 
 type CreateClientProps = {
